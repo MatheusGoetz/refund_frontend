2a4efda25866a813ffb3ed58ac693a763470f772
Matheus Goetz <goetz.dev@gmail.com>
2025-11-22 11:26:26 -0300

    feat: compartilhando layout entre rotas e estilizando o SingIn

--- src/routes/auth-routes.tsx
+++ src/routes/auth-routes.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router";
 
+import { AuthLayout } from "../components/auth-layout";
 import { SingIn } from "../pages/SignIn";
 
 export function AuthRoutes() {
 	return (
 		<Routes>
-			<Route path="/" element={<SingIn />} />
+			<Route path="/" element={<AuthLayout />}>
+				<Route path="/" element={<SingIn />} />
+			</Route>
 		</Routes>
 	);
 }
