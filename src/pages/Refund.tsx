aea33dab0dcddb67e46b19ad72b4334b24a27ede
Matheus Goetz <goetz.dev@gmail.com>
2025-11-23 21:45:37 -0300

    feat: inicializado pagina de funcionário

---
+++ src/pages/Refund.tsx
@@ -0,0 +1,20 @@
+import { Input } from "../components/input";
+import { Select } from "../components/Select";
+
+export function Refund() {
+	return (
+		<form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+			<header>
+				<h1 className="text-xl font-bold text-gray-100">
+					Solicitação de reembolso
+				</h1>
+				<p className="text-sm text-gray-200 mt-2 mb-4">
+					Dados da pessoa para solicitar reembolso
+				</p>
+			</header>
+
+			<Input required legend="Nome da solicitação" />
+			<Select required legend="Categoria" />
+		</form>
+	);
+}
